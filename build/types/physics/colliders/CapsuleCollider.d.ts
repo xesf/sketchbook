f80099c4de35bf2867e447e33873b5714a87d2ce
xesf <xesfnet@gmail.com>
2025-02-09 16:05:51 +0000

    remove build files

--- build/types/physics/colliders/CapsuleCollider.d.ts
+++
@@ -1,7 +0,0 @@
-import * as CANNON from 'cannon-es';
-import { ICollider } from '../../interfaces/ICollider';
-export declare class CapsuleCollider implements ICollider {
-    options: any;
-    body: CANNON.Body;
-    constructor(options: any);
-}
