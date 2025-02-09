89f690cd571b0f249e7ddf09f2787d4df8423ba1
xesf <xesfnet@gmail.com>
2025-02-09 16:06:53 +0000

    small shadow improvements, remove swiftaler dependency

--- src/ts/world/Sky.ts
+++ src/ts/world/Sky.ts
@@ -4,43 +4,44 @@ import { World } from './World';
 import { EntityType } from '../enums/EntityType';
 import { IUpdatable } from '../interfaces/IUpdatable';
 import { default as CSM } from 'three-csm';
+// import { CSM } from 'three/examples/jsm/csm/CSM.js';
 
 export class newSky extends THREE.Object3D implements IUpdatable
 {
-	public updateOrder: number = 5;
+    public updateOrder: number = 5;
 
-	public sunPosition: THREE.Vector3 = new THREE.Vector3();
-	public csm: CSM;
+    public sunPosition: THREE.Vector3 = new THREE.Vector3();
+    public csm: CSM;
 
-	set theta(value: number) {
-		this._theta = value;
-		this.refreshSunPosition();
-	}
+    set theta(value: number) {
+        this._theta = value;
+        this.refreshSunPosition();
+    }
 
-	set phi(value: number) {
-		this._phi = value;
-		this.refreshSunPosition();
-		this.refreshHemiIntensity();
-	}
+    set phi(value: number) {
+        this._phi = value;
+        this.refreshSunPosition();
+        this.refreshHemiIntensity();
+    }
 
-	private _phi: number = 50;
-	private _theta: number = 145;
+    private _phi: number = 50;
+    private _theta: number = 145;
 
-	private hemiLight: THREE.HemisphereLight;
-	private maxHemiIntensity: number = 0.9;
-	private minHemiIntensity: number = 0.3;
+    private hemiLight: THREE.HemisphereLight;
+    private maxHemiIntensity: number = 0.9;
+    private minHemiIntensity: number = 0.3;
 
     private sky: Sky;
-	private skyMesh: THREE.Mesh;
-	private skyMaterial: THREE.ShaderMaterial;
+    private skyMesh: THREE.Mesh;
+    private skyMaterial: THREE.ShaderMaterial;
 
-	private world: World;
+    private world: World;
 
-	constructor(world: World)
-	{
-		super();
+    constructor(world: World)
+    {
+        super();
 
-		this.world = world;
+        this.world = world;
 
         // Create sky for material
         const sky = new Sky();
@@ -50,96 +51,100 @@ export class newSky extends THREE.Object3D implements IUpdatable
         //sky.material.uniforms['mieCoefficient'].value = 0.08;
         //sky.material.uniforms['mieDirectionalG'].value = 0.8;
         sky.visible = true;
-		
-		// Sky material
-		this.skyMaterial = new THREE.ShaderMaterial({
-			uniforms: THREE.UniformsUtils.clone(sky.material.uniforms),
-			fragmentShader: sky.material.fragmentShader,
-			vertexShader: sky.material.vertexShader,
-			side: THREE.BackSide
-		});
+        
+        // Sky material
+        this.skyMaterial = new THREE.ShaderMaterial({
+            uniforms: THREE.UniformsUtils.clone(sky.material.uniforms),
+            fragmentShader: sky.material.fragmentShader,
+            vertexShader: sky.material.vertexShader,
+            side: THREE.BackSide
+        });
 
-		// Mesh
-		this.skyMesh = new THREE.Mesh(
-			new THREE.SphereGeometry(1000, 24, 12),
-			this.skyMaterial
-		);
-		this.attach(this.skyMesh);
+        // Mesh
+        this.skyMesh = new THREE.Mesh(
+            new THREE.SphereGeometry(1000, 24, 12),
+            this.skyMaterial
+        );
+        this.attach(this.skyMesh);
 
-		// Ambient light
-		this.hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1.0 );
-		this.refreshHemiIntensity();
-		this.hemiLight.color.setHSL( 0.59, 0.4, 0.6 );
-		this.hemiLight.groundColor.setHSL( 0.095, 0.2, 0.75 );
-		this.hemiLight.position.set( 0, 50, 0 );
-		this.world.graphicsWorld.add( this.hemiLight );
+        // Ambient light
+        this.hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1.0 );
+        this.refreshHemiIntensity();
+        this.hemiLight.color.setHSL( 0.59, 0.4, 0.6 );
+        this.hemiLight.groundColor.setHSL( 0.095, 0.2, 0.75 );
+        this.hemiLight.position.set( 0, 50, 0 );
+        this.world.graphicsWorld.add( this.hemiLight );
 
-		// CSM
-		// New version
-		// let splitsCallback = (amount, near, far, target) =>
-		// {
-		// 	for (let i = amount - 1; i >= 0; i--)
-		// 	{
-		// 		target.push(Math.pow(1 / 3, i));
-		// 	}
-		// };
+        // CSM
+        // New version
+        // let splitsCallback = (amount, near, far, target) =>
+        // {
+        // 	for (let i = amount - 1; i >= 0; i--)
+        // 	{
+        // 		target.push(Math.pow(1 / 3, i));
+        // 	}
+        // };
 
-		// Legacy
-		let splitsCallback = (amount, near, far) =>
-		{
-			let arr = [];
+        // Legacy
+        let splitsCallback = (amount, near, far) =>
+        {
+            let arr = [];
 
-			for (let i = amount - 1; i >= 0; i--)
-			{
-				arr.push(Math.pow(1 / 4, i));
-			}
+            for (let i = amount - 1; i >= 0; i--)
+            {
+                arr.push(Math.pow(1 / 4, i));
+            }
 
-			return arr;
-		};
+            return arr;
+        };
 
-		this.csm = new CSM({
-			//fov: 80,
-			maxFar: 250,	// maxFar
-			lightIntensity: 2.5,
-			cascades: 3,
-			shadowMapSize: 2048,
-			camera: world.camera,
-			parent: world.graphicsWorld,
-			mode: 'custom',
-			customSplitsCallback: splitsCallback
-		});
-		this.csm.fade = true;
+        this.csm = new CSM({
+            maxFar: world.camera.far,
+            lightIntensity: 1.5,
+            cascades: 4,
+            shadowMapSize: 512,
+            lightDirection: new THREE.Vector3(-this.sunPosition.x, -this.sunPosition.y, -this.sunPosition.z).normalize(),
+            camera: world.camera,
+            parent: world.graphicsWorld,
+            mode: 'custom',
+            customSplitsCallback: splitsCallback
+        });
+        this.csm.fade = false;
 
-		this.refreshSunPosition();
-		
-		world.graphicsWorld.add(this);
-		world.registerUpdatable(this);
-	}
+        this.refreshSunPosition();
+        
+        world.graphicsWorld.add(this);
+        world.registerUpdatable(this);
+    }
 
-	public update(timeScale: number): void
-	{
-		this.position.copy(this.world.camera.position);
-		this.refreshSunPosition();
+    public update(timeScale: number): void
+    {
+        this.position.copy(this.world.camera.position);
+        this.refreshSunPosition();
 
-		this.csm.update(); // Removed argument
-		this.csm.lightDirection = new THREE.Vector3(-this.sunPosition.x, -this.sunPosition.y, -this.sunPosition.z).normalize();
-	}
+        this.csm.update(); // Removed argument
+    }
+    
+    public refreshSunPosition(): void
+    {
+        const sunDistance = 10;
+        
+        this.sunPosition.x = sunDistance * Math.sin(this._theta * Math.PI / 180) * Math.cos(this._phi * Math.PI / 180);
+        this.sunPosition.y = sunDistance * Math.sin(this._phi * Math.PI / 180);
+        this.sunPosition.z = sunDistance * Math.cos(this._theta * Math.PI / 180) * Math.cos(this._phi * Math.PI / 180);
+        
+        this.skyMaterial.uniforms.sunPosition.value.copy(this.sunPosition);
+        //this.skyMaterial.uniforms.cameraPos.value.copy(this.world.camera.position);
+        // Line above throws error. Also does not appear to be called again.
+        
+        // this.csm.lightDirection = new THREE.Vector3(-this.sunPosition.x, -this.sunPosition.y, -this.sunPosition.z).normalize();
+        this.csm.lightDirection.x = -this.sunPosition.x;
+        this.csm.lightDirection.y = -this.sunPosition.y;
+        this.csm.lightDirection.z = -this.sunPosition.z;
+    }
 
-	public refreshSunPosition(): void
-	{
-		const sunDistance = 10;
-
-		this.sunPosition.x = sunDistance * Math.sin(this._theta * Math.PI / 180) * Math.cos(this._phi * Math.PI / 180);
-		this.sunPosition.y = sunDistance * Math.sin(this._phi * Math.PI / 180);
-		this.sunPosition.z = sunDistance * Math.cos(this._theta * Math.PI / 180) * Math.cos(this._phi * Math.PI / 180);
-
-		this.skyMaterial.uniforms.sunPosition.value.copy(this.sunPosition);
-		//this.skyMaterial.uniforms.cameraPos.value.copy(this.world.camera.position);
-		// Line above throws error. Also does not appear to be called again.
-	}
-
-	public refreshHemiIntensity(): void
-	{
-		this.hemiLight.intensity = this.minHemiIntensity + Math.pow(1 - (Math.abs(this._phi - 90) / 90), 0.25) * (this.maxHemiIntensity - this.minHemiIntensity);
-	}
+    public refreshHemiIntensity(): void
+    {
+        this.hemiLight.intensity = this.minHemiIntensity + Math.pow(1 - (Math.abs(this._phi - 90) / 90), 0.25) * (this.maxHemiIntensity - this.minHemiIntensity);
+    }
 }
